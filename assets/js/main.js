31d0fcb3a10b968c0798342f87a8d7bccb11c76c
khaisendefo <khaisendefo@gmail.com>
2025-01-27 20:13:23 +0300

    Created mobile version wedsite

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,15 @@ const gallerySlider = () => {
     pauseOnHover: true,
     pagination: false,
     arrows: false,
+    breakpoints: {
+      1000: {
+        perPage: 2,
+        gap: '10px',
+      },
+      700: {
+        perPage: 1,
+      }
+    }
   });
 
   splide.mount();
@@ -55,6 +64,16 @@ const reviewsSlider = () => {
     pagination: false,
     arrows: false,
     gap: '15px',
+
+    breakpoints: {
+      1000: {
+        perPage: 2,
+        gap: '15px',
+      },
+      700: {
+        perPage: 1,
+      }
+    }
   });
 
   splide.mount();
@@ -88,3 +107,52 @@ const reviewsSlider = () => {
 };
 
 reviewsSlider();
+
+const toggleMenu = () => {
+  document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector('.header-burger');
+    const nav = document.querySelector('.mobile-menu');
+    const closeButton = document.querySelector('.mobile-menu__close'); // Кнопка закрытия
+    const buttonToClose = document.querySelector('.mobile-menu__button'); // Новая кнопка для закрытия меню
+    const overlay = document.querySelector('.mobile-menu__overlay');
+    const body = document.body;
+
+    function closeNav() {
+      toggleButton.classList.remove('active');
+      nav.classList.remove('active');
+      overlay.classList.remove('active');
+      body.classList.remove('no-scroll');
+    }
+
+    toggleButton.addEventListener('click', function () {
+      const isActive = toggleButton.classList.toggle('active');
+      nav.classList.toggle('active');
+      overlay.classList.toggle('active');
+      if (isActive) {
+        body.classList.add('no-scroll');
+      } else {
+        body.classList.remove('no-scroll');
+      }
+    });
+
+    document.addEventListener('click', function(event) {
+      if (!nav.contains(event.target) && !toggleButton.contains(event.target) && !overlay.contains(event.target) && !buttonToClose.contains(event.target)) {
+        closeNav();
+      }
+    });
+
+    overlay.addEventListener('click', closeNav);
+
+    closeButton.addEventListener('click', closeNav); // Закрытие при клике на кнопку close
+
+    buttonToClose.addEventListener('click', closeNav); // Закрытие при клике на mobile-menu__button
+
+    document.addEventListener('keydown', function(event) {
+      if (event.key === 'Escape' && nav.classList.contains('active')) {
+        closeNav();
+      }
+    });
+  });
+};
+
+toggleMenu();
